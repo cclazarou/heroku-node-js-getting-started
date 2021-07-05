8034c08be182b3b21858bbed694d5bc4a829ad2b
candace <2030153+cclazarou@users.noreply.github.com>
2021-07-04 17:57:42 -0700

    show postgresql db test_table rows when visiting /db

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ express()
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/cool',(req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
   showTimes = () => {
     let result = '';
